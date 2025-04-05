dc1ecc3b5b8bd9b9928e0bde8bd71e6e65776ac3
Seu Nome <9paogigante@gamil.com>
2025-04-05 15:20:56 -0300

    inserindo novos elementos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,15 @@ import { View, Text } from "react-native";
 
 export default function index () {
 return (
-<View>
-    <Text>eu sou o pior!!</Text>
-</View>
-);
+        <View>
+        <Text>eu sou o pior!!</Text>
+        <Text>Misael</Text> 
+
+        <Text>Nacimento: 19/07/2005</Text>
+
+        <Text>Endereco: Praia do gado  </Text>
+
+    
+        </View>
+    );
 }
