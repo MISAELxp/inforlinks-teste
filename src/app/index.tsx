9ebde754c271607ac9a1cd21e7688daacad24585
Seu Nome <9paogigante@gamil.com>
2025-04-05 15:40:21 -0300

    Novos Dados

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,14 +3,19 @@ import { View, Text } from "react-native";
 export default function index () {
 return (
         <View>
-        <Text>eu sou o pior!!</Text>
-        <Text>Misael</Text> 
-
+        <Text>Meu Cadastro</Text>
+        <Text>Nome: Misael </Text> 
+        <Text>idade: 200 Anos</Text>
         <Text>Nacimento: 19/07/2005</Text>
+        <Text>Nacionalidade: Mandagáscar</Text>
+        <Text>Cidade: Boca do Acre  </Text>
+        <Text>Endereço:Praia do gado</Text>
+        <Text>Telefone: 97 000000000</Text>
+        <Text>cpf: 000 000 000-00</Text>
 
-        <Text>Endereco: Praia do gado  </Text>
 
-    
+
+
         </View>
     );
 }
